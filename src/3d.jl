92ce958e1b38c536d40783fc43fcb5e9512128d4
Felix Gerick <felixgerick@gmail.com>
2019-10-30 12:24:35 +0100

    documenter build travis sucks

--- src/3d.jl
+++ src/3d.jl
@@ -12,3 +12,19 @@ function get_ub(evecs,vs,N,cmat)
     end
     return us,bs
 end
+
+
+function run(m::ModelSetup,datapath="",SAVEDATA=false)
+    a,b,c,Le,b0 = m.a,m.b,m.c,m.Le,b.b0
+    Ω = [0,0,1/Le]
+    LHS,RHS,vs = Mire.assemblemhd(N,a,b,c,Ω,b0)
+    A,B= complex.(Matrix(RHS)),complex.(Matrix(LHS))
+    cmat = cacheint(N,a,b,c)
+    S=GenericSchur.schur(A,B)
+    ω=S.values
+    evecs=eigvecs(S)
+    us,bs=get_ub(evecs,vs,N,cmat)
+    if SAVEDATA
+        fname="/home/gerickf/data/eigenmodes_128bit_ell_N$(N).jld"
+        JLD2.@save fname A B vs S ω evecs a b c Le Ω b0 us bs N
+end
--- test/runmodels.jl
+++ test/runmodels.jl
@@ -1,6 +1,18 @@
-using Revise, Test, Elltorque, Mire
+using Revise, Test, Elltorque, Mire, DoubleFloats
 
+#sphere float64
 m1 = ModelSetup(1.,1.,1.,1e-5,b0_1_1)
 
-big1=one(BigFloat)
-m2 = ModelSetup(big1,big1,big1,big"1e-5",(a,b,c)->b0_1_1(a,b,c)+b0_1_3(a,b,c))
+#ellipsoid a,b,c=1.25,0.8,1.0
+# big1=one(BigFloat)
+# m2 = ModelSetup(big"1.25",big"0.8",big1,big"1e-5",(a,b,c)->b0_1_1(a,b,c)+b0_1_3(a,b,c))
+
+df641 = one(Double64)
+m2 = ModelSetup(df64"1.25",df64"0.8",df641,df64"1e-5",(a,b,c)->b0_1_1(a,b,c)+b0_1_3(a,b,c))
+
+
+
+@testset "Run and save models" begin
+    @test
+    # Write your own tests here.
+end
