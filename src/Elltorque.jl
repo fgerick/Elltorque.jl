e1b0b77765c0f4d9647d9029e6e0f2a7bf588aff
Felix Gerick <felixgerick@gmail.com>
2019-10-30 11:44:00 +0100

    next try

--- src/Elltorque.jl
+++ src/Elltorque.jl
@@ -1,5 +1,20 @@
 module Elltorque
 
-greet() = print("Hello World!")
+using LinearAlgebra, Statistics, Mire, GenericSchur, DoubleFloats
+
+struct ModelSetup{T <: Real}
+    a::T
+    b::T
+    c::T
+    Le::T
+    b0
+end
+
+ModelSetup(a::T,b::T,c::T,Le::T,b0fun::Function) where T <: Real = ModelSetup{T}(a,b,c,Le,b0fun(a,b,c))
+
+#selected mean magnetic fields satisfying b₀⋅n=0 and ∇⋅b₀=0 from Wu & Roberts, 2011.
+
+
+
 
 end # module
