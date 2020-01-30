9f704d6b1e11e597e234b438dd0c2eb2c71cfd44
Felix Gerick <felixgerick@gmail.com>
2020-01-30 19:16:15 +0100

    change les

--- src/runcalclehnert_parallel.jl
+++ src/runcalclehnert_parallel.jl
@@ -61,7 +61,7 @@ modeldim = parse(Int,ARGS[6])
 
     T=Double64
     D=typeof(MDIM)
-    Les=df64"10.0".^range(-7,0,length=remotecall_fetch(()->nle, 1) )
+    Les=df64"10.0".^range(-5,-1,length=remotecall_fetch(()->nle, 1) )
     # Les=10.0.^range(-7,-1,length=remotecall_fetch(()->nle,1))
 end
 
